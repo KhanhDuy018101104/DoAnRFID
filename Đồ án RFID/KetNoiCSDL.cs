c276454949de57c16b1c382b3fb6c65fc7d5b1a8
khánh duy <txtcfvn@gmail.com>
2022-12-08 12:28:47 +0700

    new update

---
+++ Đồ án RFID/KetNoiCSDL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Đồ_án_RFID
+{
+    class KetNoiCSDL
+    {
+        private static SqlConnection cnn = new SqlConnection();
+        public static void MoKetNoi()
+        {
+            try
+            {
+                string sqlcon = @"Data Source = DESKTOP - S4EE5LP\DUY; Initial Catalog = GymRFID; Integrated Security = True";
+                cnn.ConnectionString = sqlcon;
+                if (cnn.State == ConnectionState.Closed)
+                    cnn.Open();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public static void DongKetNoi()
+        {
+            if (cnn.State == ConnectionState.Open)
+                cnn.Close();
+        }
+        public static DataTable ExcuteQuery(string sql)
+        {
+            MoKetNoi();
+            SqlCommand cd = new SqlCommand(sql, cnn);
+            SqlDataReader dr = cd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            DongKetNoi();
+            return dt;
+        }
+        public static void ExcuteNonQuery(string sql)
+        {
+            MoKetNoi();
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            cmd.ExecuteNonQuery();
+            DongKetNoi();
+        }
+    }
+}
+
---
+++ Đồ án RFID/ThongTinKH.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Đồ_án_RFID
+{
+    class ThongTinKH
+    {
+        public static DataTable ThongTinKhachHang(string mathe)
+        {
+            string sql = "SELECT * FROM KHACHHANG WHERE MaThe = " + mathe;
+            DataTable dt = new DataTable();
+            dt = KetNoiCSDL.ExcuteQuery(sql);
+            return dt;
+        }
+
+        public static void InsertThoiGianQuetThe(string mathe, string tenkh, string thoigian)
+        {
+            string sql = "INSERT INTO LICHSUQUET (MaThe, TenKH, ThoiGian) VALUES('" + mathe + "','" + tenkh + "','" + thoigian + "')";
+            KetNoiCSDL.ExcuteNonQuery(sql);
+        }
+
+        public static DataTable LichSuQuetThe(string mathe)
+        {
+            string sql = "";
+            if (mathe == "999999999")
+                sql = "SELECT * FROM LICHSUQUET";
+            else sql = "SELECT * FROM LICHSUQUET WHERE MaThe = " + mathe + " ORDER BY ThoiGian DESC";
+            DataTable dt = new DataTable();
+            dt = KetNoiCSDL.ExcuteQuery(sql);
+            return dt;
+        }
+
+        public static DataTable GetTenKH()
+        {
+            string sql = "SELECT DISTINCT TenKH, MaThe FROM KHACHHANG ORDER BY TenKH";
+            DataTable dt = new DataTable();
+            dt = KetNoiCSDL.ExcuteQuery(sql);
+            return dt;
+        }
+
+        public static void DangKyTheMoi(string mathe, string makh, string tenkh, string gioitinh, string sdt, string cmnd, string diachi, string ngaysinh)
+        {
+            string sql = "INSERT INTO KHACHHANG (MaThe, MaKH, TenKH, GioiTinh, SĐT, CMND, Diachi, NgaySinh) VALUES ('"
+                + mathe + "','" + makh + "','" + tenkh + "','" + gioitinh + "','" + sdt + "','" + cmnd + "','" + diachi + "','" + ngaysinh + "')";
+            KetNoiCSDL.ExcuteNonQuery(sql);
+        }
+
+        public static void CapNhatThe(string mathe, string makh, string tenkh, string gioitinh, string sdt, string cmnd, string diachi, string ngaysinh)
+        {
+            string sql = "UPDATE KHACHHANG SET MaKH = '" + makh + "', TenKH = '" + tenkh +
+                "', GioiTinh = '" + gioitinh + "', SĐT = '" + sdt + "', CMND = '" + cmnd + "', Diachi = '" + diachi + "', NgaySinh = '" + ngaysinh + "' WHERE MaThe = '" + mathe + "'";
+            KetNoiCSDL.ExcuteNonQuery(sql);
+        }
+
+        public static void XoaThe(string mathe)
+        {
+            string sql = "DELETE FROM KHACHHANG WHERE MaThe = '" + mathe + "'";
+            KetNoiCSDL.ExcuteNonQuery(sql);
+        }
+
+        public static DataTable GetAllKH()
+        {
+            string sql = "SELECT * FROM KHACHHANG";
+            DataTable dt = new DataTable();
+            dt = KetNoiCSDL.ExcuteQuery(sql);
+            return dt;
+        }
+    }
+}
+
+   
+
